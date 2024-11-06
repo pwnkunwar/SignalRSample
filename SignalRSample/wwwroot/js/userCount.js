28721bc298892be9362d186170ec9cb000821741
pwn1kunwar <pwn1kunwar@gmail.com>
2024-11-06 10:26:13 +0545

    Added Cient Side Configuration of SignalR

---
+++ SignalRSample/wwwroot/js/userCount.js
@@ -0,0 +1,25 @@
+﻿
+//create connection
+var connectionUserCount = new signalR.HubConnectionBuilder.withUrl("/hubs/userCount").build();
+
+//connect to methods that hub invokes aka receive notifications from hub
+connectionUserCount.on("updateTotalViews", (Value) => {
+    var newCpuntSpan = document.getElementById("totalViewsCounter");
+    newCpuntSpan.innerText = value.toString();
+});
+
+//invoke hub methods aka send notifications to hub
+function newWindowLoadedOnClient() {
+    connectionUserCount.Send("NewWindowLoaded");
+}
+
+
+// start connection
+function fuflfilled() {
+    console.log("Connection to User Hub Successful");
+}
+function rejected() {
+
+}
+
+connectionUserCount.start().then(fuflfilled, rejected)
