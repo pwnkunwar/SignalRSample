fe580d426f8a952ec21f493ffd532dc828e676fd
pwn1kunwar <pwn1kunwar@gmail.com>
2024-11-29 23:23:52 +0545

    Update SignalR functionality and refine Home views and scripts

--- SignalRSample/wwwroot/js/userCount.js
+++ SignalRSample/wwwroot/js/userCount.js
@@ -1,25 +1,30 @@
 ﻿
 //create connection
-var connectionUserCount = new signalR.HubConnectionBuilder.withUrl("/hubs/userCount").build();
+var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
 
 //connect to methods that hub invokes aka receive notifications from hub
-connectionUserCount.on("updateTotalViews", (Value) => {
+connectionUserCount.on("updateTotalViews", (value) => {
     var newCpuntSpan = document.getElementById("totalViewsCounter");
+    newCpuntSpan.innerText = value.toString();
+});
+connectionUserCount.on("updateTotalUsers", (value) => {
+    var newCpuntSpan = document.getElementById("totalUsersCounter");
     newCpuntSpan.innerText = value.toString();
 });
 
 //invoke hub methods aka send notifications to hub
 function newWindowLoadedOnClient() {
-    connectionUserCount.Send("NewWindowLoaded");
+    connectionUserCount.send("NewWindowLoaded");
 }
 
 
 // start connection
 function fuflfilled() {
+    newWindowLoadedOnClient();
     console.log("Connection to User Hub Successful");
 }
 function rejected() {
-
+    console.log("Connection to the User Hub UnSuccessful");
 }
 
 connectionUserCount.start().then(fuflfilled, rejected)
