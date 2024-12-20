57ce8c829ba5f283c4bd3a5066e43b04d15c66d3
pwn1kunwar <pwn1kunwar@gmail.com>
2024-12-20 13:44:45 +0545

    adding deathyHallow.js

---
+++ SignalRSample/wwwroot/js/deathyHallow.js
@@ -0,0 +1,32 @@
+﻿var cloakSpan = document.getElementById("cloakCounter");
+var stoneSpan = document.getElementById("stoneCounter");
+var wandSpan = document.getElementById("wandCounter");
+
+// Create connection
+var connectionDeathlyHallows = new signalR.HubConnectionBuilder()
+    .withUrl("/hubs/deathyhallows")
+    .build();
+
+connectionDeathlyHallows.on("updateDeathlyHallowCount", (cloak, stone, wand) => {
+    cloakSpan.innerText = cloak.toString();
+    stoneSpan.innerText = stone.toString();
+    wandSpan.innerText = wand.toString();
+});
+
+// Start connection
+function fulfilled() {
+    connectionDeathlyHallows.invoke("GetRaceStatus").then((raceCounter) => {
+        cloakSpan.innerText = raceCounter.cloak.toString();
+        stoneSpan.innerText = raceCounter.stone.toString();
+        wandSpan.innerText = raceCounter.wand.toString();
+    }).catch((error) => {
+        console.error("Error invoking GetRaceStatus:", error);
+    });
+    console.log("Connection to User Hub Successful");
+}
+
+function rejected() {
+    console.log("Connection to the User Hub Unsuccessful");
+}
+
+connectionDeathlyHallows.start().then(fulfilled, rejected);
