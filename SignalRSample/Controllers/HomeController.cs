96e46a3559760f46683e21a990eb42c607ace564
pwn1kunwar <pwn1kunwar@gmail.com>
2024-12-20 13:41:56 +0545

    dependency injection of IHubContext in HomeController

--- SignalRSample/Controllers/HomeController.cs
+++ SignalRSample/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalRSample.Hubs;
 using SignalRSample.Models;
 using System.Diagnostics;
 
@@ -7,15 +9,30 @@ namespace SignalRSample.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IHubContext<DeathlyHallowsHub> _deathlyHub;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+           IHubContext<DeathlyHallowsHub> deathlyHub)
         {
             _logger = logger;
+            _deathlyHub = deathlyHub;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+        public async Task<IActionResult> DeathlyHallows(string type)
+        {
+            if(SD.DealthyHallowRace.ContainsKey(type))
+            {
+                SD.DealthyHallowRace[type]++;
+            }
+            await _deathlyHub.Clients.All.SendAsync("updateDealthyHallowCount",
+                SD.DealthyHallowRace[SD.Cloak],
+                SD.DealthyHallowRace[SD.Cloak],
+                SD.DealthyHallowRace[SD.Wand]);
+            return Accepted();
         }
 
         public IActionResult Privacy()
